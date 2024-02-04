d5346a4a58b730f5bf42769a421f56f24a2074cb
Luis-ms24 <3996472@alu.murciaeduca.es>
2024-02-04 14:05:31 +0100

    Añadir función frotar que genera frases aleatorias desde un fichero de texto

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-from bayeta import frotar
-print("Hola, mundo")
+from bayeta import frotar # Importamos la función frotar del fichero bayeta.py
+
+n = 3 # Elegimos el número de frases que queremos obtener
+frases = frotar(n) # Llamamos a la función frotar con el número de frases y guardamos la lista de frases que nos devuelve
+
+for frase in frases: # Recorremos la lista de frases
+    print(frase) # Imprimimos cada frase por la salida estándar
--- bayeta.py
+++ bayeta.py
@@ -1,2 +1,9 @@
+import random # Importamos el módulo random de Python
+
 def frotar(n_frases: int = 1) -> list():
-    pass # Esta función está sin definir
+ 
+    frases = [] # Creamos una lista vacía donde guardaremos las frases generadas
+    with open("frases.txt", "r") as f: # Abrimos el fichero de texto que contiene las frases posibles, en modo lectura
+        frases_posibles = f.read().splitlines() # Leemos el contenido del fichero y lo dividimos por líneas, guardando cada frase en una lista
+    frases = random.sample(frases_posibles, n_frases) # Elegimos N frases al azar de la lista de frases, sin repetición, y las guardamos en la lista de frases
+    return frases # Devolvemos la lista de frases
